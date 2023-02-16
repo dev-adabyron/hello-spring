d39551989bc3a2373593ad69d80a34d38e00957b
dy <dev.adabyron@gmail.com>
2023-02-17 07:14:28 +0900

    회원 웹 기능 - 홈화면, 등록, 조회

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -6,6 +6,11 @@ import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -14,5 +19,28 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService=memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+
+    }
+    @PostMapping("/members/new")
+    public String createMember(MemberForm memberForm){
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        System.out.println(memberForm.getName());
+        memberService.Join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String membersList(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
+
     }
 }
